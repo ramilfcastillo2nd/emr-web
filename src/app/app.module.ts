715f814435abef59da5e414ed4b6ccd043be5b18
Ramil Ferrer Castillo <ramil@kathykuohome.com>
2025-02-07 13:56:46 +0800

    updated packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppLayoutModule } from './layout/app.layout.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
